9bdc998a7269fc16949997593f140b1e5fff4c6d
Elsa Letellier <elsa.letellier.44@gmail.com>
2025-06-09 20:42:18 +0200

    feat: connection to bdd

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+import { createConnection } from 'mysql2';
+import dotenv from 'dotenv';
+
+// Crée une connexion
+const connection = createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Connexion
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion :', err.stack);
+    return;
+  }
+  console.log('Connecté à MySQL avec l’ID de connexion ' + connection.threadId);
+});
+
+export default connection;
