38bd75a8e906b171315a3ee543bf32db3b6e3b5d
hrakkaz <rakkazhafsa@gmail.com>
2025-07-18 14:35:32 +0200

    Ajout des modifs depuis develop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import MovieCardDetails from './Components/MovieCardDetails';
+import UserProfile from './Components/UserProfile';
+import RealisateurDetails from './Components/RealisateurDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Routes>
+          {/* Route dynamique qui passe l'id en paramètre */}
+          <Route path="/film/:id" element={<MovieCardDetails />} />
+          <Route path="/realisateur" element={<RealisateurDetails />} />
+          {/* Route pour le profil utilisateur */}
+          {/* <Route path="/profile" element={<UserProfile />} /> */}
+
+          {/* Route par défaut ou autre page d'accueil */}
+          {/* <Route path="*" element={<div>Page non trouvée</div>} /> */}
+        </Routes>
+      </div>
+
   );
 }
 
 export default App;
+
+
+
