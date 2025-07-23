c6a8b6db27ba50dd2a986b150e7ead4c9a9b41d3
Stadiane Metino <smetinongoufo@myges.fr>
2025-07-23 10:45:32 +0200

    Modif route pour recup les films

--- backend/routes/film.js
+++ backend/routes/film.js
@@ -1,21 +1,16 @@
-import express from 'express';
-import connection from '../db.js';
+import express from "express";
+import connection from "../db.js";
 
 const router = express.Router();
 
-// Route pour récupérer un film selon son ID
-router.get('/:id', async (req, res) => {
-  const filmId = req.params.id;
+// Route pour récupérer tous les films
+router.get("/", async (req, res) => {
   try {
-    const [rows] = await connection.query('SELECT * FROM Film WHERE Id_Film = ?', [filmId]);
-    if (rows.length > 0) {
-      res.json(rows[0]);
-    } else {
-      res.status(404).json({ error: 'Film non trouvé' });
-    }
+    const [rows] = await connection.query("SELECT * FROM Film");
+    res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Erreur serveur' });
+    res.status(500).json({ error: "Erreur serveur" });
   }
 });
 
